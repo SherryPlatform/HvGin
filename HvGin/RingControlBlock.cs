b10a32c93340d8efe1dd4da5c6be086319876f54
MouriNaruto <Kenji.Mouri@outlook.com>
2025-01-12 22:52:35 +0800

    Add RingControlBlock struct definition to HvGin.

---
+++ HvGin/RingControlBlock.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.InteropServices;
+
+namespace HvGin
+{
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+    internal struct RingControlBlock
+    {
+        public uint In;
+
+        public uint Out;
+
+        public uint InterruptMask;
+
+        public uint PendingSendSize;
+
+        public unsafe fixed uint Reserved[12];
+
+        public uint FeatureBitsValue;
+
+        public bool SupportsPendingSendSize
+        {
+            get
+            {
+                return (FeatureBitsValue & 1) != 0;
+            }
+            set
+            {
+                if (value)
+                {
+                    FeatureBitsValue |= 1;
+                }
+                else
+                {
+                    FeatureBitsValue &= ~Convert.ToUInt32(1);
+                }
+            }
+        }
+    };
+}
