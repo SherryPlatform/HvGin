082b4139b1e36b18c6796ac9a242d9189ca01181
MouriNaruto <Kenji.Mouri@outlook.com>
2025-01-12 22:53:10 +0800

    Add PacketDescriptor struct definition to HvGin.

---
+++ HvGin/PacketDescriptor.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace HvGin
+{
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+    internal struct PacketDescriptor
+    {
+        public ushort Type;
+
+        public ushort DataOffset8;
+
+        public ushort Length8;
+
+        public ushort Flags;
+
+        public ulong TransactionId;
+    };
+}
