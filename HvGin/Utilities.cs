0e55253ccb5cf2ad9ea7e6acd3bca38742a38ece
MouriNaruto <Kenji.Mouri@outlook.com>
2025-01-13 22:37:34 +0800

    Add Utilities.GetAlignedSize method to HvGin.

---
+++ HvGin/Utilities.cs
@@ -0,0 +1,12 @@
+﻿namespace HvGin
+{
+    internal class Utilities
+    {
+        public static int GetAlignedSize(
+            int Size,
+            int Alignment)
+        {
+            return (Size + Alignment - 1) & ~(Alignment - 1);
+        }
+    }
+}
