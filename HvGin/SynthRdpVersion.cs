c9b19f597de59643f2cf2fbe4eb210b114b3370a
MouriNaruto <Kenji.Mouri@outlook.com>
2025-01-15 20:36:54 +0800

    Add SynthRdpVersion struct definition to HvGin.

---
+++ HvGin/SynthRdpVersion.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+
+namespace HvGin
+{
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+    internal struct SynthRdpVersion
+    {
+        public uint AsDWORD;
+
+        public ushort MajorVersion
+        {
+            get
+            {
+                return Convert.ToUInt16(AsDWORD & 0x0000FFFF);
+            }
+            set
+            {
+                AsDWORD |= Convert.ToUInt32(value);
+            }
+        }
+
+        public ushort MinorVersion
+        {
+            get
+            {
+                return Convert.ToUInt16((AsDWORD & 0xFFFF0000) >> 16);
+            }
+            set
+            {
+                AsDWORD |= Convert.ToUInt32(value << 16);
+            }
+        }
+    }
+}
