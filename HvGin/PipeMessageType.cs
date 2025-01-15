81cff2f7b9f3a70b67dd4c7deedb10621063e8be
MouriNaruto <Kenji.Mouri@outlook.com>
2025-01-15 20:33:12 +0800

    Add PipeMessageType enum definition to HvGin.

---
+++ HvGin/PipeMessageType.cs
@@ -0,0 +1,12 @@
+﻿namespace HvGin
+{
+    internal enum PipeMessageType : uint
+    {
+        Invalid = 0,
+        Data = 1,
+        Partial = 2,
+        SetupGpaDirect = 3,
+        TeardownGpaDirect = 4,
+        IndicationComplete = 5,
+    }
+}
