00c7483277ff5653de82113a1f3518af6a205613
MouriNaruto <Kenji.Mouri@outlook.com>
2025-01-15 20:36:44 +0800

    Add SynthRdpHeader struct definition to HvGin.

---
+++ HvGin/SynthRdpHeader.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+
+namespace HvGin
+{
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+    internal struct SynthRdpHeader
+    {
+        private uint RawType;
+
+        public int Size;
+
+        public SynthRdpMessageType Type
+        {
+            get
+            {
+                return Enum.IsDefined(typeof(SynthRdpMessageType), RawType)
+                    ? (SynthRdpMessageType)RawType
+                    : SynthRdpMessageType.Error;
+            }
+            set
+            {
+                RawType = Convert.ToUInt32(value);
+            }
+        }
+    }
+}
