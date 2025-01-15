369b1e62633d3353b77c92a2faa6d391c3a9d837
MouriNaruto <Kenji.Mouri@outlook.com>
2025-01-15 20:34:03 +0800

    Add Utilities.BytesToStructure method to HvGin.

--- HvGin/Utilities.cs
+++ HvGin/Utilities.cs
@@ -1,4 +1,7 @@
-﻿namespace HvGin
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace HvGin
 {
     internal class Utilities
     {
@@ -7,6 +10,18 @@
             int Alignment)
         {
             return (Size + Alignment - 1) & ~(Alignment - 1);
+        }
+
+        public static T BytesToStructure<
+            [DynamicallyAccessedMembers(
+                DynamicallyAccessedMemberTypes.PublicConstructors |
+                DynamicallyAccessedMemberTypes.NonPublicConstructors)] T>(
+            byte[] Bytes) where T : struct
+        {
+            GCHandle Handle = GCHandle.Alloc(Bytes, GCHandleType.Pinned);
+            T Result = Marshal.PtrToStructure<T>(Handle.AddrOfPinnedObject());
+            Handle.Free();
+            return Result;
         }
     }
 }
