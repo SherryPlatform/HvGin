397507456cb3481bb2f4f19c805b155450374f2b
MouriNaruto <Kenji.Mouri@outlook.com>
2025-01-15 20:39:27 +0800

    Add SynthRdp struct definition to HvGin.

---
+++ HvGin/SynthRdp.cs
@@ -0,0 +1,23 @@
+﻿namespace HvGin
+{
+    internal class SynthRdp
+    {
+        public static readonly string ControlClassId =
+            "f8e65716-3cb3-4a06-9a60-1889c5cccab5";
+
+        public static readonly string ControlInstanceId =
+            "99221fa0-24ad-11e2-be98-001aa01bbf6e";
+
+        public static readonly string DataClassId =
+            "f9e9c0d3-b511-4a48-8046-d38079a8830c";
+
+        public static readonly string[] DataInstanceIds =
+        {
+            "99221fa1-24ad-11e2-be98-001aa01bbf6e",
+            "99221fa2-24ad-11e2-be98-001aa01bbf6e",
+            "99221fa3-24ad-11e2-be98-001aa01bbf6e",
+            "99221fa4-24ad-11e2-be98-001aa01bbf6e",
+            "99221fa5-24ad-11e2-be98-001aa01bbf6e"
+        };
+    }
+}
