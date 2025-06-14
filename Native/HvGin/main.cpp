47c7e3d977bcd89172528ddfd19d6b8abf53c684
MouriNaruto <Kenji.Mouri@outlook.com>
2025-06-15 01:59:14 +0800

    Add the simple demo for HvGin native.

--- Native/HvGin/main.cpp
+++ Native/HvGin/main.cpp
@@ -1,7 +1,26 @@
-﻿#include <cstdio>
+﻿#include <Mile.Mobility.Portable.Types.h>
+#include <Mile.HyperV.VMBus.h>
+
+#include <cpuid.h>
+
+#include <cstdio>
+#include <cstring>
 
 int main()
 {
-    printf("%s say hello to you!\n", "HvGin");
+    HV_CPUID_RESULT HvCpuIdResult;
+    std::memset(&HvCpuIdResult, 0, sizeof(HV_CPUID_RESULT));
+    __cpuid(
+        HvCpuIdFunctionHvInterface,
+        HvCpuIdResult.Eax,
+        HvCpuIdResult.Ebx,
+        HvCpuIdResult.Ecx,
+        HvCpuIdResult.Edx);
+    if (HvMicrosoftHypervisorInterface == HvCpuIdResult.HvInterface.Interface)
+    {
+        std::printf("HvGin is running on Microsoft Hyper-V!\n");
+    }
+
+    std::printf("%s say hello to you!\n", "HvGin");
     return 0;
 }
