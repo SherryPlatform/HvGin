f213b32a411816ec5c5934bca686a3e6b95f32c1
MouriNaruto <Kenji.Mouri@outlook.com>
2025-06-17 22:26:05 +0800

    Introduce Mile.Posix.Helpers and add Mile.Helpers.CppBase.

--- Native/HvGin/main.cpp
+++ Native/HvGin/main.cpp
@@ -1,6 +1,7 @@
 ﻿#define MILE_MOBILITY_ENABLE_MINIMUM_SAL
 #include <Mile.Mobility.Portable.Types.h>
 #include <Mile.HyperV.VMBus.h>
+#include <Mile.Helpers.CppBase.h>
 
 #include <unistd.h>
 #include <errno.h>
@@ -10,81 +11,8 @@
 
 #include <cpuid.h>
 
-#include <cstdarg>
 #include <cstdio>
 #include <cstring>
-
-#include <string>
-
-namespace Mile
-{
-    /**
-     * @brief Write formatted data to a onebyte or multibyte string, suggested
-     *        encoding with UTF-8.
-     * @param Format Format-control string.
-     * @param ArgList Pointer to list of optional arguments to be formatted.
-     * @return A formatted string if successful, an empty string otherwise.
-    */
-    std::string VFormatString(
-        _In_ char const* const Format,
-        _In_ va_list ArgList)
-    {
-        int Length = 0;
-
-        // Get the length of the format result.
-        {
-            va_list CurrentArgList;
-            va_copy(CurrentArgList, ArgList);
-            Length = std::vsnprintf(nullptr, 0, Format, CurrentArgList);
-            va_end(CurrentArgList);
-        }
-        if (Length > 0)
-        {
-            // Allocate for the format result.
-            std::string Buffer;
-            Buffer.resize(static_cast<std::size_t>(Length));
-
-            // Format the string.
-            {
-                va_list CurrentArgList;
-                va_copy(CurrentArgList, ArgList);
-                Length = std::vsnprintf(
-                    &Buffer[0],
-                    Buffer.size() + 1,
-                    Format,
-                    CurrentArgList);
-                va_end(CurrentArgList);
-            }
-            if (Length > 0)
-            {
-                // If succeed, resize to fit and return result.
-                Buffer.resize(static_cast<std::size_t>(Length));
-                return Buffer;
-            }
-        }
-
-        // If failed, return an empty string.
-        return std::string();
-    }
-
-    /**
-     * @brief Write formatted data to a onebyte or multibyte string, suggested
-     *        encoding with UTF-8.
-     * @param Format Format-control string.
-     * @param ... Optional arguments to be formatted.
-     * @return A formatted string if successful, an empty string otherwise.
-    */
-    std::string FormatString(
-        _In_ char const* const Format,
-        ...)
-    {
-        va_list ArgList;
-        va_start(ArgList, Format);
-        std::string Result = Mile::VFormatString(Format, ArgList);
-        va_end(ArgList);
-        return Result;
-    }
-}
 
 EXTERN_C int MOAPI HvGinMountPlan9ReadOnlyShareWithHyperVSocket(
     _In_ MO_UINT32 Port,
